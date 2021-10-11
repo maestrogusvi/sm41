27eecc61a6f317b89033fc3b5dee619a6d8f2dec
armando vivanco <armando.vivanco@sapper.ai>
2021-10-11 08:49:47 -0500

    Feat(VivancoDTO) new DTO

---
+++ src/main/java/com/ut/sm41/dto/VivancoDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm41.dto;
+
+public class VivancoDTO {
+
+    int id;
+    String name;
+
+    public VivancoDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
