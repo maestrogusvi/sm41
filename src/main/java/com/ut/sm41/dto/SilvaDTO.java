5df284b78a823738b9afc23df06e8c5cbfec35c1
Andres199602 <71568045+Andres199602@users.noreply.github.com>
2021-10-11 08:56:38 -0500

    feat (SilvaDTO) HttpService

---
+++ src/main/java/com/ut/sm41/dto/SilvaDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm41.dto;
+
+public class SilvaDTO {
+
+    int id;
+    String name;
+
+    public SilvaDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
