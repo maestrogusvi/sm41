60d59de4f8d2d3aef9a3e72731ac6f5f09ec3f2f
Jessica Yanet Zapata Soberanis <zapatajessica084@gmail.com>
2021-10-12 02:39:23 -0500

    ZapataDTO

---
+++ src/main/java/com/ut/sm41/dto/ZapataDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm41.dto;
+
+public class ZapataDTO {
+
+        int id;
+        String name;
+
+        public ZapataDTO() {
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+}
