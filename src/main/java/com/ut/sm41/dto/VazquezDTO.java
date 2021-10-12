33a2facd2d4a3df1a305d27dac67de1bc608f271
FAVA <fava.flashpoint@gmail.com>
2021-10-11 22:40:36 -0500

    Feat(VazquezDTO) new DTO

---
+++ src/main/java/com/ut/sm41/dto/VazquezDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm41.dto;
+
+public class VazquezDTO {
+
+    int id;
+    String name;
+
+    public VazquezDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
