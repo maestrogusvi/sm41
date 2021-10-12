e931f8ee9a186580fa3c718fd6578616bef3aa24
SrFlix <flixgamer450@gmail.com>
2021-10-12 18:43:35 -0500

    Feat(MontejoDTO) new DTO

---
+++ src/main/java/com/ut/sm41/dto/MontejoDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm41.dto;
+
+public class MontejoDTO {
+
+    int id;
+    string name;
+
+    public MontejoDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+}
