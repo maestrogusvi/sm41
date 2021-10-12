ffa9b2b036de243acadc0dc4547b63af1f606ed1
lestradavaz <20393136@utcancun.edu.mx>
2021-10-12 11:53:29 -0500

    Feat(EstradaDTO) new DTO

---
+++ src/main/java/com/ut/sm41/dto/EstradaDTO.java
@@ -0,0 +1,18 @@
+package com.ut.sm41.dto;
+
+public class EstradaDTO {
+
+    int id;
+    String name;
+
+    public EstradaDTO() {}
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+}
