ce1cc79762996e8c30c7e51503e8c6e9a470bcc8
chrisxk22 <k36_pumas@hotmail.com>
2021-10-12 09:48:35 -0500

    Feat(RamirezDTO) new DTO

---
+++ src/main/java/com/ut/sm41/dto/RamirezDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm41.dto;
+
+public class RamirezDTO {
+
+    int id;
+    String name;
+
+    public RamirezDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
