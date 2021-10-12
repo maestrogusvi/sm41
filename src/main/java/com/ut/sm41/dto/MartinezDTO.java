85cbb5ee20f3e8b8bc751f939bffd4bf3ead42c2
Fractalxd <gaelml22@gmail.com>
2021-10-12 18:44:35 -0500

    Feat(MartinezDTO) new DTO

---
+++ src/main/java/com/ut/sm41/dto/MartinezDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm41.dto;
+
+public class MartinezDTO {
+    int id;
+    string name;
+
+    public MartinezDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+}
+
