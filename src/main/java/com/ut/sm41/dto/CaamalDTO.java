b9718f853ee93ce3bde4ac3f8b51be23771d8b02
AntonioLH-Caamal <caamaljesus0@gmail.com>
2021-10-12 20:21:26 -0500

    feat (CaamalDTO) HttpService

---
+++ src/main/java/com/ut/sm41/dto/CaamalDTO.java
@@ -0,0 +1,27 @@
+package com.ut.sm41.dto;
+
+public class CaamalDTO {
+
+    int id;
+    String name;
+
+    public CaamalDTO() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
