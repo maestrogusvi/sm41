cf75c9f939f0a39d7b5c690da3c25d155225d9e6
GMann0 <gregoriohau02@gmail.com>
2021-10-13 13:57:33 -0500

    Feat(HauDTO) new DTO

---
+++ src/main/java/com/ut/sm41/dto/HauDTO.java
@@ -0,0 +1,25 @@
+package com.ut.sm41.dto;
+
+public class HauDTO {
+    int id;
+    String name;
+
+    public HauDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
