bd7c1799d8b7623f9c1f8edba77ca0fff89e2062
EmilianoTC <20393117@utcancun.edu.mx>
2021-10-13 01:54:52 -0500

    Feat(TunDTO) new DTO

---
+++ src/main/java/com/ut/sm41/dto/TunDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm41.dto;
+
+public class TunDTO {
+
+    int id;
+    String name;
+
+    public TunDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
