6e5df9d4fb4ed2a9db58e785aa10af43c0ae0708
FerAM02 <ferariasmay@gmail.com>
2021-10-14 15:59:45 -0500

    Feat(AriasDTO) new DTO

---
+++ src/main/java/com/ut/sm41/dto/AriasDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm41.dto;
+
+public class AriasDTO {
+
+    int id;
+    String name;
+
+    public AriasDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
