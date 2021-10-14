a334ceacaa5d95b7a896693ae726db86d8c6126f
J1307-bb <edgarjair2001@hotmail.com>
2021-10-14 07:38:03 -0500

    Feat(BadilloDTO) new DTO

---
+++ src/main/java/com/ut/sm41/dto/BadilloDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm41.dto;
+
+public class BadilloDTO {
+
+    int id;
+    String name;
+
+    public BadilloDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
