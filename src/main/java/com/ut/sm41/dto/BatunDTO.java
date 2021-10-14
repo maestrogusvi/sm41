798fb4d3574d9185cd280ca3c2ba48acde6c7925
Emmanuel_Batun <emanuelbatun419@gmail.com>
2021-10-14 01:34:02 -0500

    feat(BatunDTO) new DTO

---
+++ src/main/java/com/ut/sm41/dto/BatunDTO.java
@@ -0,0 +1,26 @@
+package com.ut.sm41.dto;
+
+public class BatunDTO {
+
+    int id;
+    String name;
+
+    public BatunDTO(){
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
