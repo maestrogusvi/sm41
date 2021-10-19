65bd374e23818dc826e08e9f13d37a518c9cfffb
MisaelVarguez <misaelvarguez100@gmail.com>
2021-10-19 16:30:19 -0500

    fix(ApplicationController)add new endpoint BatunHttp

--- src/main/java/com/ut/sm41/service/ApplicationService.java
+++ src/main/java/com/ut/sm41/service/ApplicationService.java
@@ -2,10 +2,7 @@ package com.ut.sm41.service;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.ut.sm41.dto.BadilloDTO;
-import com.ut.sm41.dto.BatunDTO;
-import com.ut.sm41.dto.BeeceptorDTO;
-import com.ut.sm41.dto.MartinezDTO;
+import com.ut.sm41.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
@@ -57,6 +54,16 @@ public class ApplicationService {
         badilloDTO.setId(json.get("id").getAsInt());
         badilloDTO.setStatus(json.get("status").getAsString());
         return badilloDTO;
+    }
+
+    public VarguezDTO varguezHttp() throws IOException {
+        JsonParser parser = new JsonParser();
+        JsonObject json = (JsonObject) parser.parse(httpService.sendRequestHttpS("https://misael41.free.beeceptor.com","GET",null,null,"json",null, null));
+        VarguezDTO varguezDTO = new VarguezDTO();
+       varguezDTO.setName(json.get("name").getAsString());
+        varguezDTO.setId(json.get("id").getAsInt());
+        varguezDTO.setStatus(json.get("status").getAsString());
+        return varguezDTO;
     }
 }
 
