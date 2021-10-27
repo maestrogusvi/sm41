250a3547be98aaae40021a68b000d3b8b4ebbcd0
hannia24 <hannia13_isabel@hotmail.com>
2021-10-27 13:48:51 -0500

    Fix(ApplicationController) add new endpoint AnotaHttp

--- src/main/java/com/ut/sm41/controller/ApplicationController.java
+++ src/main/java/com/ut/sm41/controller/ApplicationController.java
@@ -25,7 +25,7 @@ public class ApplicationController {
     @GetMapping("/")
     public String inicio() {
         return applicationService.firstService();
-        
+
     }
 
     @GetMapping("/testHttp")
