7d254c27b4733a9e1df28aed02008d90a57702f8
lestradavaz <20393136@utcancun.edu.mx>
2021-11-11 08:55:44 -0500

    add(PlayDTO) add API

--- src/main/java/com/ut/sm41/service/ApplicationService.java
+++ src/main/java/com/ut/sm41/service/ApplicationService.java
@@ -2,10 +2,7 @@ package com.ut.sm41.service;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.ut.sm41.dto.BeeceptorDTO;
-import com.ut.sm41.dto.EstradaDTO;
-import com.ut.sm41.dto.RamirezDTO;
-import com.ut.sm41.dto.HauDTO;
+import com.ut.sm41.dto.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -24,6 +21,8 @@ public interface ApplicationService {
      EstradaDTO estradaHttp() throws IOException ;
      EstradaDTO estradaPostHttp(EstradaDTO estradaDTO) throws IOException;
      void testMyFirstObject(EstradaDTO estradaDTO);
+
+     PlayDTO playHttp() throws IOException;
 
      RamirezDTO ramirezHttp() throws IOException ;
      RamirezDTO ramirezPostHttp(RamirezDTO ramirezDTO) throws IOException;
