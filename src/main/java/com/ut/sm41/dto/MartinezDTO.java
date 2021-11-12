bdc54b36b3ff2caafd34a1e0097f89d7465d8d29
Fractalxd <gaelml22@gmail.com>
2021-11-12 07:54:40 -0500

    Feat(MartinezDTO) update

--- src/main/java/com/ut/sm41/dto/MartinezDTO.java
+++ src/main/java/com/ut/sm41/dto/MartinezDTO.java
@@ -32,5 +32,22 @@ public class MartinezDTO {
     public void setStatus(String status) {
         this.status = status;
     }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+    public String toJSON() {
+        return "{" +
+                "\"id\":" + id + "," +
+                "\"name\":\"" + name + "\"" +
+                "\"status\":\"" + status + "\"" +
+                '}';
+    }
 }
 
