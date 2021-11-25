084f9ab8e2e8940a291df3bf2238f81a767eba7b
lestradavaz <20393136@utcancun.edu.mx>
2021-11-25 08:26:06 -0500

    Added(EstradaDTO)

---
+++ src/main/java/com/ut/sm41/dto/EstradaDTO.java
@@ -0,0 +1,42 @@
+package com.ut.sm41.dto;
+
+public class EstradaDTO {
+
+    int id;
+    String name;
+    String status;
+
+    public EstradaDTO() {}
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String toString() {
+        return "{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+    public String toJson() {
+        return "{" +
+                "\"id\":" + id + "," +
+                " \"name\":\"" + name + "\"," +
+                " \"status\":\"" + status +"\""+
+                '}';
+    }
+}
