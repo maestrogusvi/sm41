b170134b63ac48a836b5a108ec22404678562b0e
Andrés silva <andy_227_07@hotmail.com>
2021-11-30 17:48:32 -0500

    Local

--- src/main/java/com/ut/sm41/repository/UserRepository.java
+++ src/main/java/com/ut/sm41/repository/UserRepository.java
@@ -1,12 +1,9 @@
 package com.ut.sm41.repository;
 
 
-import com.ut.sm41.model.User;
 import com.ut.sm41.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Integer >{ }
